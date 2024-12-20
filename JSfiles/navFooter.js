0594dd37f151b8b7bd7fde37cae5d6e86784c155
HebaOmarr <Hebaomarr9@gmail.com>
2024-12-20 13:29:21 +0200

    NameOf user

--- JSfiles/navFooter.js
+++ JSfiles/navFooter.js
@@ -1,17 +1,27 @@
 let listcart = JSON.parse(localStorage.getItem("listcart")) || {};
-
+function getCookie(name) {
+  const cookies = document.cookie.split("; ");
+  for (let cookie of cookies) {
+    const [key, value] = cookie.split("=");
+    if (key === name) {
+      return value;
+    }
+  }
+  return null;
+}
 function loadNavbar() {
 let counttotal=0;
   Object.values(listcart).forEach((product) => {
     counttotal+=product.cn;
   });
+
   fetch("../HTMLfiles/navbar.html")
     .then((response) => response.text())
     .then((data) => {
-
+      
       document.querySelector("header").innerHTML = data;
       document.querySelector(".cart-count").innerHTML=Number(counttotal);
-
+     document.querySelector(".Nam").innerHTML = getCookie("userName");
 
     });
 }
